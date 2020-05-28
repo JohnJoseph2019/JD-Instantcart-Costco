ff39e4539b11083483c19b52f28a0cf542383c4f
Juan Delgado <Delgado-juan@hotmail.com>
2020-05-27 22:55:38 -0400

    was able to add the links correctly for mobile and for the menu to click away when you click on the link

--- client/src/components/StoreFront/HamburgerNav.jsx
+++ client/src/components/StoreFront/HamburgerNav.jsx
@@ -5,72 +5,94 @@ import { hamburgerLink } from "../selectedOptions"
 import './HamburgerNav.css'
 
 export default class HamburgerNav extends Component {
+ constructor(props) {
+  super(props)
+  this.state = {
+   menuOpen: false
+  }
+ }
+ handleClick = event => {
+  event.preventDefault();
+  if (this.state.menuOpen === false) {
+   console.log("handleClick", this.state.menuOpen)
+   this.setState({ menuOpen: true });
+  }
+ }
+ // }
+ handleMenuClick = (event, property, value) => {
+  event.preventDefault();
+  this.setState({ menuOpen: false }, this.props.callbackFromParent(hamburgerLink(this.props.products, property, value)));
+
+ }
  render() {
-  console.log("In HamburgerNav ", this.props.callbackFromParent)
   return (
-   <div className="hamburger-menu">
-    <Menu>
+   <div className="hamburger-menu" onClick={this.handleClick}>
+    <Menu
+     isOpen={this.state.menuOpen}
+    >
      <h1 className="departments-header">Departments</h1>
      <ul className="parent-menu-hamburger">
 
-      <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'department', "Meat & Seafood"))}>Meat & Seafood</Link>
+      <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'department', "Meat & Seafood")}>Meat & Seafood</Link>
 
        <ul className="parent-sub-menu-hamburger">
 
-        <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Prime Beef"))}>Prime Beef</Link></li>
+        <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Prime Beef")}>Prime Beef</Link></li>
 
-        <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Pork & Lamb"))}>Pork & Lamb</Link></li>
+        <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Pork & Lamb")}>Pork & Lamb</Link></li>
 
-        <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Beef"))}>Beef</Link></li>
+        <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Beef")}>Beef</Link></li>
 
-        <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Seafood"))}>Seafood</Link></li>
+        <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Seafood")}>Seafood</Link></li>
 
-        <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Poultry"))}>Poultry</Link></li>
+        <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Poultry")}>Poultry</Link></li>
 
        </ul>
 
       </li>
 
-      <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'department', "Produce"))}>Produce</Link>
+      <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'department', "Produce")}>Produce</Link>
 
        <ul className="parent-sub-menu-hamburger">
 
-        <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Fruit & Vegetable Trays"))}>Fruit & Vegetable</Link></li>
+        <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Fruit & Vegetable Trays")}>Fruit & Vegetable</Link></li>
 
-        <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Fruit"))}>Fruit</Link></li>
+        <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Fruit")}>Fruit</Link></li>
 
-        <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Vegetable"))}>Vegetables</Link></li>
+        <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Vegetable")}>Vegetables</Link></li>
 
-        <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Salad Mix"))}>Salad Mix</Link></li>
+        <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Salad Mix")}>Salad Mix</Link></li>
 
        </ul></li>
 
-      <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'department', "Deli"))}>Deli</Link></li>
+      <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'department', "Deli")}>Deli</Link></li>
 
       <ul className="parent-sub-menu-hamburger">
 
-       <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Meat & Cheese Trays"))}>Meat & Cheese</Link></li>
+       <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Meat & Cheese Trays")}>Meat & Cheese</Link></li>
 
-       <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Prepared Meals"))}>Prepared Meals</Link></li>
+       <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Prepared Meals")}>Prepared Meals</Link></li>
 
-       <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Prepared Soups & Salad"))}>Soups & Salad</Link></li>
+       <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Prepared Soups & Salad")}>Soups & Salad</Link></li>
 
-       <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Dips & Spreads"))}>Dips & Spreads</Link></li>
+       <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Dips & Spreads")}>Dips & Spreads</Link></li>
       </ul>
 
-      <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'department', "Bakery & Desserts"))}>Bakery & Desserts</Link>
+      <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'department', "Bakery & Desserts")}>Bakery & Desserts</Link>
 
        <ul className="parent-sub-menu-hamburger">
 
-        <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Bread"))}>Bread</Link></li>
+        <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Bread")}>Bread</Link></li>
 
-        <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Buns & Rolls"))}>Buns & Rolls</Link></li>
+        <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Buns & Rolls")}>Buns & Rolls</Link></li>
 
-        <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Cookies"))}>Cookies</Link></li>
+        <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Cookies")}>Cookies</Link></li>
 
-        <li><Link className="department-item-hamburger" to="#" onClick={() => this.props.callbackFromParent(hamburgerLink(this.props.products, 'category', "Pies & Cakes"))}>Pies & Cakes</Link></li>
+        <li><Link className="department-item-hamburger" to="#" onClick={(e) => this.handleMenuClick(e, 'category', "Pies & Cakes")}>Pies & Cakes</Link></li>
 
        </ul>
+
+       <li><Link className="department-item" to="#" onClick={(e) => this.handleMenuClick(e, 'none', 'none')}>Back to Main</Link></li>
 
       </li>
      </ul>
